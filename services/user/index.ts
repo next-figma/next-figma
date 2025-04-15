2bd5c933bdc5e6e686c04a866ce66dd89c714e1d
yuriivintoniak <you@example.com>
2025-04-15 19:22:09 +0300

    feat(services): add api function to update user

--- services/user/index.ts
+++ services/user/index.ts
@@ -9,3 +9,10 @@ export const getAllUsers = async (): Promise<IUser[]> => {
   const { data } = await httpClient.get(PATH_PREFIX);
   return data;
 };
+
+export const UPDATE_USER_BY_ID = "UPDATE_USER_BY_ID";
+
+export const updateUser = async (id: string, updatedData: Partial<IUser>): Promise<IUser> => {
+  const { data } = await httpClient.patch(`${PATH_PREFIX}/${id}`, updatedData);
+  return data;
+};
